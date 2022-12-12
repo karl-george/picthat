6c8fa0d0de2db09d2a4f02115317fc5daf269566
Karl George <karlgdev@gmail.com>
2022-12-12 17:06:37 +0100

    Populating categories for pictures

---
+++ picthat_frontend/src/utils/data.js
@@ -0,0 +1,42 @@
+export const categories = [
+  {
+    name: 'Animals',
+    image: '',
+  },
+  {
+    name: 'Art',
+    image: '',
+  },
+  {
+    name: 'Cars',
+    image: '',
+  },
+  {
+    name: 'Food',
+    image: '',
+  },
+  {
+    name: 'Games',
+    image: '',
+  },
+  {
+    name: 'Photography',
+    image: '',
+  },
+  {
+    name: 'Technology',
+    image: '',
+  },
+  {
+    name: 'Travel',
+    image: '',
+  },
+  {
+    name: 'Wallpapers',
+    image: '',
+  },
+  {
+    name: 'Other',
+    image: '',
+  },
+];
