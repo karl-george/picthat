77aede3cffdaf15b18ba6df2706c59df3297af4a
Karl George <karlgdev@gmail.com>
2022-12-12 17:06:45 +0100

    Styling sidebar

--- picthat_frontend/src/components/Sidebar.jsx
+++ picthat_frontend/src/components/Sidebar.jsx
@@ -1,6 +1,14 @@
 import { NavLink, Link } from 'react-router-dom';
 
+import { categories } from '../utils/data';
+
 import logo from '../assets/logo-no-background.png';
+
+const isActiveStyle =
+  'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
+
+const isNotActiveStyle =
+  'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 
 const Sidebar = ({ closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -18,6 +26,27 @@ const Sidebar = ({ closeToggle }) => {
           <img src={logo} alt='logo' className='w-16' />
           <p className='text-xl md:text-3xl text-accent'>PicThat</p>
         </Link>
+        <div className='flex flex-col gap-5'>
+          <NavLink
+            to='/'
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleCloseSidebar}
+          >
+            Discover
+          </NavLink>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+            >
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
       </div>
     </div>
   );
