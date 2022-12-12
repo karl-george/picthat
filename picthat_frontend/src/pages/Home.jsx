b0c232f4492ac45b2f267ebc30896c1612197775
Karl George <karlgdev@gmail.com>
2022-12-12 16:26:19 +0100

    Setting up Sidebar

--- picthat_frontend/src/pages/Home.jsx
+++ picthat_frontend/src/pages/Home.jsx
@@ -1,14 +1,23 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Link, Routes, Route } from 'react-router-dom';
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+
+import UserProfile from '../components/UserProfile';
+import Sidebar from '../components/Sidebar';
+import Wall from './Wall';
 
 import { client } from '../client';
 import { fetchUser } from '../utils/fetchUser';
 import { userQuery } from '../utils/queries';
 
 import logo from '../assets/logo-no-background.png';
+import { useInsertionEffect } from 'react';
 
 const Home = () => {
   const [user, setUser] = useState();
+  const [toggleSidebar, setToggleSidebar] = useState(false);
+  const scrollRef = useRef(null);
 
   // Fetch user from localstorage
   const userInfo = fetchUser();
@@ -22,10 +31,43 @@ const Home = () => {
     });
   }, []);
 
+  useEffect(() => {
+    scrollRef.current.scrollTo(0, 0);
+  }, []);
+
   return (
-    <div>
-      <h1>{user?.userName}</h1>
-      <img src={user?.image} />
+    <div className='flex bg-gray-500 md:flex-row flex-col h-screen'>
+      <div className='hidden md:flex h-screen flex-initial'>
+        <Sidebar />
+      </div>
+      <div className='flex md:hidden flex-row'>
+        <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+          <HiMenu
+            fontSize={40}
+            onClick={() => setToggleSidebar(true)}
+            className='cursor-pointer'
+          />
+        </div>
+        {toggleSidebar && (
+          <div className='fixed w-4/5 bg-white h-screen overflow-y-auto shadow-md z-10'>
+            <div className='absolute w-full flex justify-end items-center p-2'>
+              <AiFillCloseCircle
+                fontSize={30}
+                className='cursor-pointer'
+                onClick={setToggleSidebar(false)}
+              />
+            </div>
+            <Sidebar closeToggle={setToggleSidebar} />
+          </div>
+        )}
+      </div>
+
+      <div className='pb-2 flex-1 h-screen overflow-y-scroll' ref={scrollRef}>
+        <Routes>
+          <Route path='/user/:userId' element={<UserProfile />} />
+          <Route path='/*' element={<Wall user={user && user} />} />
+        </Routes>
+      </div>
     </div>
   );
 };
