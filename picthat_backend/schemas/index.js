af0adccc1896685670778ae276f237dd3b8058d0
Karl George <karlgdev@gmail.com>
2022-12-12 12:23:29 +0100

    Adding postedBy schema

--- picthat_backend/schemas/index.js
+++ picthat_backend/schemas/index.js
@@ -2,5 +2,6 @@ import user from './user'
 import post from './post'
 import comment from './comment'
 import save from './save'
+import postedBy from './postedBy'
 
-export const schemaTypes = [user, post, comment, save]
+export const schemaTypes = [user, post, comment, save, postedBy]
