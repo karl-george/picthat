cf4d3eaaa04ddce16a3c9434859ff66e43c1ea51
Karl George <karlgdev@gmail.com>
2022-12-12 15:05:16 +0100

    Checking user queries and logging in

--- picthat_frontend/src/pages/Home.jsx
+++ picthat_frontend/src/pages/Home.jsx
@@ -1,5 +1,31 @@
+import { useState, useEffect } from 'react';
+import { Link, Routes, Route } from 'react-router-dom';
+
+import { client } from '../client';
+import { fetchUser } from '../utils/fetchUser';
+import { userQuery } from '../utils/queries';
+
+import logo from '../assets/logo-no-background.png';
+
 const Home = () => {
-  return <div>Home</div>;
+  const [user, setUser] = useState();
+
+  const userInfo = fetchUser();
+
+  useEffect(() => {
+    const query = userQuery(userInfo?.sub);
+
+    client.fetch(query).then((data) => {
+      setUser(data[0]);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>{user?.userName}</h1>
+      <img src={user?.image} />
+    </div>
+  );
 };
 
 export default Home;
