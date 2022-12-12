642ea3627313f7c567fb7bd03693737cf1c96051
Karl George <karlgdev@gmail.com>
2022-12-12 11:54:41 +0100

    Adding user schema

---
+++ picthat_backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'user',
+  type: 'document',
+  title: 'User',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UserName',
+      type: 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string',
+    },
+  ],
+}
