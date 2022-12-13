8f93628aab97afe49c9eb64877ffcf4c9b052302
Karl George <karlgdev@gmail.com>
2022-12-13 12:47:08 +0100

    Implementing Posts component

--- picthat_frontend/src/components/Posts.jsx
+++ picthat_frontend/src/components/Posts.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import Spinner from './Spinner';
+
+import { client } from '../client';
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState(null);
+
+  return <div>{loading && <Spinner msg={'Adding ideas to your wall'} />}</div>;
 };
 
 export default Posts;
