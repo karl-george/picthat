4083b87e8871fa0302e952b1559f25815a7fed8b
Karl George <karlgdev@gmail.com>
2022-12-13 11:40:16 +0100

    Working on navbar

--- picthat_frontend/src/components/Navbar.jsx
+++ picthat_frontend/src/components/Navbar.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdSearch } from 'react-icons/io';
 
-const Navbar = () => {
-  return <div>Navbar</div>;
+const Navbar = ({ user }) => {
+  return (
+    <nav className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
+      <div className=''>
+        <IoMdSearch />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
