756fbf21306a9bebeca127e64c3472dba77975d1
Karl George <karlgdev@gmail.com>
2022-12-13 13:41:13 +0100

    Adding postedby link to bottom of images

--- picthat_frontend/src/components/Post.jsx
+++ picthat_frontend/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { client, urlFor } from '../client';
 import { fetchUser } from '../utils/fetchUser';
@@ -11,8 +12,23 @@ const Post = ({ post: { postedBy, image, _id, url, save } }) => {
   return (
     <div className='m-2'>
       <div>
-        <img src={urlFor(image).width(250).url()} />
+        <img
+          src={urlFor(image).width(250).url()}
+          alt='user post'
+          className='rounded-lg w-full'
+        />
       </div>
+      <Link
+        to={`user/${postedBy?._id}`}
+        className='flex flex-row gap-2 mt-1 items-center'
+      >
+        <img
+          src={postedBy.image}
+          alt='user profile'
+          className='w-8 h-8 rounded-full object-cover'
+        />
+        <p className='font-semibold capitalize'>{postedBy.userName}</p>
+      </Link>
     </div>
   );
 };
