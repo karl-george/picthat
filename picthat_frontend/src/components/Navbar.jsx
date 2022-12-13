61da10d07199df088737956aa2c80c14c84414f2
Karl George <karlgdev@gmail.com>
2022-12-13 12:16:09 +0100

    Styling navbar

--- picthat_frontend/src/components/Navbar.jsx
+++ picthat_frontend/src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { IoMdSearch } from 'react-icons/io';
 
-const Navbar = ({ user, searchTerm, setSearchTerm }) => {
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
   const navigate = useNavigate();
 
   return (
     <nav className='flex gap-2 md:gap-5 w-full mt-5 pb-7'>
+      <Link to='create-post' className='bg-accent text-white rounded-lg p-3'>
+        Create
+      </Link>
       <div className='flex justify-start items-center w-full px-2 rounded-md bg-white border-none outline-none focus-within:shadow-sm'>
         <IoMdSearch fontSize={21} className='ml-1' />
         <input
@@ -17,6 +20,13 @@ const Navbar = ({ user, searchTerm, setSearchTerm }) => {
           className='p-2 w-full bg-white outline-none'
         />
       </div>
+      <Link to={`user/${user?._id}`} className='md:block hidden'>
+        <img
+          src={user?.image}
+          alt='user icon'
+          className='w-14 h-12 rounded-lg'
+        />
+      </Link>
     </nav>
   );
 };
