b4d4ac0efca73117c4a6febb1a731e345f26668a
Karl George <karlgdev@gmail.com>
2022-12-13 12:35:15 +0100

    Working on populating posts when using search

--- picthat_frontend/src/components/Search.jsx
+++ picthat_frontend/src/components/Search.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { client } from '../client';
+import { postsQuery, searchQuery } from '../utils/queries';
 
 import Spinner from './Spinner';
 
-const Search = ({ searchTerm, setSearchTerm, user }) => {
-  const [loading, setLoading] = useState(true);
+const Search = ({ searchTerm, setSearchTerm }) => {
+  const [loading, setLoading] = useState(false);
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    // If user has entered a search string populate with relevant posts
+    // else just show standard posts
+    if (searchTerm) {
+      setLoading(true);
+
+      const query = searchQuery(searchTerm.toLowerCase());
+
+      client.fetch(query).then((data) => {
+        setPosts(data);
+        setLoading(false);
+      });
+    } else {
+      client.fetch(postsQuery).then((data) => {
+        setPosts(data);
+        setLoading(false);
+      });
+    }
+  }, [searchTerm]);
 
   return <div>{loading && <Spinner msg='Searching for posts' />}</div>;
 };
--- picthat_frontend/src/utils/queries.js
+++ picthat_frontend/src/utils/queries.js
@@ -3,3 +3,53 @@ export const userQuery = (userId) => {
 
   return query;
 };
+
+export const searchQuery = (searchTerm) => {
+  const query = `*[_type == "post" && title match '${searchTerm}*' || category match '${searchTerm}*' || about match '${searchTerm}*']{
+    image {
+      asset -> {
+        url
+      }
+    },
+    _id,
+    url,
+    postedBy -> {
+      _id,
+      userName,
+      image
+    },
+    save[] {
+      _key,
+      postedBy -> {
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+
+  return query;
+};
+
+export const postsQuery = `*[_type == "post"] | order(_createdAt desc) {
+  image{
+    asset->{
+      url
+    }
+  },
+      _id,
+      url,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+      save[]{
+        _key,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+      },
+    } `;
