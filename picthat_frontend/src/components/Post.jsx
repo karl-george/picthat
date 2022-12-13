a74b2274bc9bb8c7ba3440a5e3a1b2d35b23abfb
Karl George <karlgdev@gmail.com>
2022-12-13 13:48:00 +0100

    Styling images

--- picthat_frontend/src/components/Post.jsx
+++ picthat_frontend/src/components/Post.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { client, urlFor } from '../client';
 import { fetchUser } from '../utils/fetchUser';
@@ -7,27 +7,34 @@ import { fetchUser } from '../utils/fetchUser';
 const Post = ({ post: { postedBy, image, _id, url, save } }) => {
   const [isHovered, setIsHovered] = useState(false);
 
+  const navigate = useNavigate();
   const user = fetchUser();
 
   return (
     <div className='m-2'>
-      <div>
+      <div
+        className='relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-out'
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onClick={() => navigate(`/post-details/${_id}`)}
+      >
         <img
           src={urlFor(image).width(250).url()}
           alt='user post'
           className='rounded-lg w-full'
         />
+        {console.log(isHovered)}
       </div>
       <Link
         to={`user/${postedBy?._id}`}
-        className='flex flex-row gap-2 mt-1 items-center'
+        className='flex flex-row gap-2 mt-1 mb-5 items-center'
       >
         <img
           src={postedBy.image}
           alt='user profile'
-          className='w-8 h-8 rounded-full object-cover'
+          className='w-6 h-6 rounded-full object-cover'
         />
-        <p className='font-semibold capitalize'>{postedBy.userName}</p>
+        <p className='text-gray-600 capitalize'>{postedBy.userName}</p>
       </Link>
     </div>
   );
