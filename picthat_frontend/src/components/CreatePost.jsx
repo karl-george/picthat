206443236f25d14eb8ab9e8070cefbf7db8ea3cb
Karl George <karlgdev@gmail.com>
2022-12-14 14:09:23 +0100

    Checking image type is correct logic

--- picthat_frontend/src/components/CreatePost.jsx
+++ picthat_frontend/src/components/CreatePost.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { client } from '../client';
 import Spinner from './Spinner';
@@ -11,6 +12,26 @@ const CreatePost = ({ user }) => {
   const [url, setUrl] = useState('');
   const [loaading, setLoaading] = useState(false);
   const [category, setCategory] = useState(null);
+  const [image, setImage] = useState(null);
+  const [wrongImageType, setWrongImageType] = useState(false);
+
+  const navigate = useNavigate();
+
+  const uploadImage = (e) => {
+    const { type, name } = e.target.files[0];
+
+    // Check image type is correct else wrong image type
+    if (
+      type === 'image/png' ||
+      type === 'image/svg' ||
+      type === 'image/jpeg' ||
+      type === 'image/gif' ||
+      type === 'image/tiff'
+    ) {
+    } else {
+      setWrongImageType(true);
+    }
+  };
 
   return <div>CreatePost</div>;
 };
