6c8fa4d468189a3c6ad60ffd4b524352d1fbefb6
Karl George <karlgdev@gmail.com>
2022-12-14 14:03:44 +0100

    Setting up creating post

--- picthat_frontend/src/components/CreatePost.jsx
+++ picthat_frontend/src/components/CreatePost.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useState } from 'react';
 
-const CreatePost = () => {
+import { client } from '../client';
+import Spinner from './Spinner';
+
+import { categories } from '../utils/data';
+
+const CreatePost = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [url, setUrl] = useState('');
+  const [loaading, setLoaading] = useState(false);
+  const [category, setCategory] = useState(null);
+
   return <div>CreatePost</div>;
 };
 
