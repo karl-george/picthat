24afa1797f66fa2fc6ca235775c6b417272825c8
Karl George <karlgdev@gmail.com>
2022-12-14 14:17:01 +0100

    Styling image upload area

--- picthat_frontend/src/components/CreatePost.jsx
+++ picthat_frontend/src/components/CreatePost.jsx
@@ -10,7 +10,7 @@ const CreatePost = ({ user }) => {
   const [title, setTitle] = useState('');
   const [about, setAbout] = useState('');
   const [url, setUrl] = useState('');
-  const [loaading, setLoaading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [category, setCategory] = useState(null);
   const [image, setImage] = useState(null);
   const [wrongImageType, setWrongImageType] = useState(false);
@@ -28,12 +28,41 @@ const CreatePost = ({ user }) => {
       type === 'image/gif' ||
       type === 'image/tiff'
     ) {
+      setWrongImageType(false);
+      setLoaading(true);
+
+      client.assets
+        .upload('image', e.target.files[0], {
+          contentType: type,
+          filename: name,
+        })
+        .then((doc) => {
+          setImage(doc);
+          setLoaading(false);
+        })
+        .catch((err) => {
+          console.log('Image upload error', err);
+        });
     } else {
       setWrongImageType(true);
     }
   };
 
-  return <div>CreatePost</div>;
+  return (
+    <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
+      <div className='flex lg:flex-row flex-col justify-center'>
+        <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
+          <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
+            {loading && <Spinner />}
+            {wrongImageType && (
+              <p>Please upload an image of the correct type</p>
+            )}
+            {!image ? <div></div> : <div></div>}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
