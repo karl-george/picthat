59be0342df8c705769d5030e431c48b1198d05b8
Karl George <karlgdev@gmail.com>
2022-12-14 14:26:41 +0100

    Finishing styling the upload area

--- picthat_frontend/src/components/CreatePost.jsx
+++ picthat_frontend/src/components/CreatePost.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+import { BsUpload } from 'react-icons/bs';
+
 import { client } from '../client';
 import Spinner from './Spinner';
 
@@ -29,7 +31,7 @@ const CreatePost = ({ user }) => {
       type === 'image/tiff'
     ) {
       setWrongImageType(false);
-      setLoaading(true);
+      setLoading(true);
 
       client.assets
         .upload('image', e.target.files[0], {
@@ -38,7 +40,7 @@ const CreatePost = ({ user }) => {
         })
         .then((doc) => {
           setImage(doc);
-          setLoaading(false);
+          setLoading(false);
         })
         .catch((err) => {
           console.log('Image upload error', err);
@@ -50,14 +52,34 @@ const CreatePost = ({ user }) => {
 
   return (
     <div className='flex flex-col justify-center items-center mt-5 lg:h-4/5'>
-      <div className='flex lg:flex-row flex-col justify-center'>
+      <div className='flex lg:flex-row flex-col justify-center items-center bg-white lg:p-5 p-3 lg:w-4/5 w-full'>
         <div className='bg-secondaryColor p-3 flex flex-0.7 w-full'>
           <div className='flex justify-center items-center flex-col border-2 border-dotted border-gray-300 p-3 w-full h-420'>
             {loading && <Spinner />}
             {wrongImageType && (
               <p>Please upload an image of the correct type</p>
             )}
-            {!image ? <div></div> : <div></div>}
+            {!image ? (
+              <label className='w-full h-full'>
+                <div className='flex flex-col items-center justify-center h-full'>
+                  <div className='flex flex-col justify-center items-center'>
+                    <p className='font-bold text-2xl'>
+                      <BsUpload />
+                    </p>
+                    <p>Click to upload</p>
+                  </div>
+                  <p></p>
+                </div>
+                <input
+                  type='file'
+                  name='upload-image'
+                  onChange={uploadImage}
+                  className='w-0 h-0'
+                />
+              </label>
+            ) : (
+              <div></div>
+            )}
           </div>
         </div>
       </div>
