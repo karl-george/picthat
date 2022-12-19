87c64e6972e6e53c39f6801a4bc4d581240ac4c9
Karl George <karlgdev@gmail.com>
2022-12-19 12:52:11 +0100

    Styling post details

--- picthat_frontend/src/components/PostDetails.jsx
+++ picthat_frontend/src/components/PostDetails.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 
+import { BsCloudDownload, BsBookmark, BsBookmarkFill } from 'react-icons/bs';
+
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
 
@@ -19,7 +21,6 @@ const PostDetails = () => {
     if (query) {
       client.fetch(query).then((data) => {
         setPostDetails(data[0]);
-        console.log(data[0]);
       });
     }
   };
@@ -31,9 +32,36 @@ const PostDetails = () => {
   if (!postDetails) return <Spinner msg='Loading image...' />;
 
   return (
-    <div>
-      <img src={postDetails.image.asset.url} alt='post image' />
-      <h1>{postDetails.title}</h1>
+    <div
+      className='flex xl:flex-row flex-col m-auto bg-white'
+      style={{ maxWidth: '1500px', borderRadius: '32px' }}
+    >
+      <div className='flex justify-center items-center md:items-start flex-initial'>
+        <img
+          src={postDetails?.image && urlFor(postDetails.image).url()}
+          alt='users post'
+          className='rounded-t-3xl rounded-b-lg'
+        />
+      </div>
+      <div className='w-full p-5 flex-1 xl:min-w-620'>
+        <div className='flex items-center justify-between'>
+          <div className='flex gap-2 items-center'>
+            <a
+              href={`${postDetails.image?.asset?.url}?dl=`}
+              download
+              onClick={(e) => e.stopPropagation()}
+              className='bg-white w-9 h-9 rounded-full flex items-center justify-center text-dark text-xl opacity-75 hover:opacity-100 hover:shadow-md outline-none'
+            >
+              <BsCloudDownload />
+            </a>
+          </div>
+        </div>
+        <div>
+          <h1 className='text-4xl font-bold break-words mt-3'>
+            {postDetails.title}
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
