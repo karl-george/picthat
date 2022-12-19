9c727fe7efeef6131fc0752acb2754aa72d96598
Karl George <karlgdev@gmail.com>
2022-12-19 12:17:33 +0100

    Setting up pin details page

--- picthat_frontend/src/components/PostDetails.jsx
+++ picthat_frontend/src/components/PostDetails.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { v4 as uuidv4 } from 'uuid';
+
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+import { client, urlFor } from '../client';
 
 const PostDetails = () => {
+  const [pinDetails, setPinDetails] = useState(null);
+
+  if (!pinDetails) return <Spinner msg='Loading pin...' />;
+
   return <div>PostDetails</div>;
 };
 
