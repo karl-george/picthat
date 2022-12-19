ed80e1de5ded02c4439b29aa9ecf3078ccf1d059
Karl George <karlgdev@gmail.com>
2022-12-19 15:31:22 +0100

    Styling user profile

--- picthat_frontend/src/components/UserProfile.jsx
+++ picthat_frontend/src/components/UserProfile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 
 import MasonryLayout from './MasonryLayout';
 import Spinner from './Spinner';
@@ -11,6 +11,7 @@ const UserProfile = () => {
   const [user, setUser] = useState(null);
 
   const { userId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const query = userQuery(userId);
@@ -20,9 +21,39 @@ const UserProfile = () => {
     });
   }, [userId]);
 
+  const logOut = () => {
+    localStorage.clear();
+
+    navigate('/login');
+  };
+
   if (!user) return <Spinner msg='Loading profile...' />;
 
-  return <div>UserProfile</div>;
+  return (
+    <div className='pb-2 h-full justify-center items-center mt-4'>
+      <div className='flex flex-col pb-5'>
+        <div className='flex flex-col justify-center items-center'>
+          <img
+            src={user?.image}
+            alt='User icon'
+            className='w-32 h-32 rounded-full object-cover shadow-xl'
+          />
+          <h1 className='font-bold text-3xl text-center mt-6'>
+            {user?.userName}
+          </h1>
+          {userId === user._id && (
+            <button
+              type='button'
+              onClick={logOut}
+              className='bg-accent text-white rounded-full px-6 py-2 font-semibold text-base outline-none mt-6'
+            >
+              Log Out
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserProfile;
